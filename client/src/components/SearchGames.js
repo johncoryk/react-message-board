efa634bc55344577166b1a9c3a3e628dc93d08d3
johncoryk <coryk0789@gmail.com>
2020-08-28 13:09:36 -0400

    basic info on game rendered on page

--- client/src/components/SearchGames.js
+++ client/src/components/SearchGames.js
@@ -13,7 +13,10 @@ const SearchGames = () => {
     e.preventDefault();
     fetch(`https://api.rawg.io/api/games?page_size=5&search=${searchItem}%20v`)
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => {
+        setGame(data.results[0]);
+        console.log(data.results[0]);
+      });
   };
 
   return (
@@ -22,6 +25,19 @@ const SearchGames = () => {
         <input type='text' value={searchItem} onChange={handleChange} />
         <input className='btn-style' type='submit' value='Search' />
       </form>
+      {game ? (
+        <article className='game-info'>
+          <h2>{game.name}</h2>
+          <h3>Released: {game.released}</h3>
+          <h3>Rating: {game.rating}</h3>
+          {game.genres.map(genre => (
+            <span>{genre.name} </span>
+          ))}
+        </article>
+      ) : (
+        'Search for a game!'
+      )}
+      <article className='game-info'></article>
     </div>
   );
 };
