bf76bc85ede06ccb8ea6c410b926b924fbc78fed
johncoryk <coryk0789@gmail.com>
2020-08-30 08:42:41 -0400

    added scaffold of mainpage with boards table

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import LargeHeading from '../components/utility/LargeHeading';
 import SubHeading from '../components/utility/SubHeading';
-// import GameHeading from './utility/GameHeading';
+import GameHeading from './utility/GameHeading';
 // import Button from '../components/utility/Button';
 
 export default class MainPage extends Component {
@@ -18,10 +18,28 @@ export default class MainPage extends Component {
       <>
         <main>
           <LargeHeading text='Boards' />
-          <div className='horizontal-rule'></div>
-          {this.state.allBoards.map(board => (
-            <SubHeading text={board.title} />
-          ))}
+          <section className='boards-table'>
+            <div className='table-header'>
+              <SubHeading text='Board' />
+              <div className='board-info'>
+                <SubHeading text='Topics' />
+                <SubHeading text='Msgs' />
+                <SubHeading text='Last Post' />
+              </div>
+              {this.state.allBoards.map(board => (
+                <>
+                  <div className='board-row'>
+                    <GameHeading text={board.title} />
+                    <div className='board-row-info'>
+                      <p>{board.topics_count}</p>
+                      <p>530</p>
+                      <p>4 Minutes</p>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+          </section>
         </main>
       </>
     );
