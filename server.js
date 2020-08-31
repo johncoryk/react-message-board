7a7109d820b563bed8d339b0e5262f77c3c28f35
johncoryk <coryk0789@gmail.com>
2020-08-31 10:15:08 -0400

    cleaned up code

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 const express = require('express');
 const logger = require('morgan');
-const path = require('path'); 
+const path = require('path');
 const passport = require('passport');
-const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
-
-
-
-
-
 
 const postRouter = require('./routes/post-router');
 
 const app = express();
 
-
-
 //Middleware
-
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-/////////Jon add
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(
   session({
     key: process.env.SECRET_KEY,
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: true,
-  }),
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -40,11 +31,7 @@ app.use(express.static('public'));
 app.use((req, res, next) => {
   console.log('-------', req.user, req.path);
   next();
-})
-//////////////
-
-
-
+});
 
 //Setting up PORT
 const PORT = process.env.PORT || 3000;
