cccfaa23559d5aa6d1279292aaa7578df06f4ef3
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-09-01 12:19:21 -0500

    post controller finished

--- routes/post-router.js
+++ routes/post-router.js
@@ -1,6 +1,12 @@
+const express = require('express')
 const postRouter = require('express').Router();
 const postController = require('../controllers/post-controller');
 
 postRouter.get('/', postController.index);
+postRouter.post('/', postController.create);
+
+postRouter.get('/:id', postController.show);
+postRouter.put('/:id', postController.update);
+postRouter.delete('/:id',postController.delete);
 
 module.exports = postRouter;
