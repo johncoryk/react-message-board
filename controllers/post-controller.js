ef954b1d2a416e91c1b96c8ad5e3d65ccb816eaf
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-09-01 12:16:52 -0500

    post controller methods done

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,14 +1,70 @@
 const Post = require('../models/Post.js');
 
-const postController = {
-  index: (req, res) => {
-    Post.getAll().then(posts => {
-      res.send({
+const postController = {};
+
+postController.index = (req, res, next) => {
+  Post.getAll()
+    .then((posts) => {
+      res.json({
         message: 'ok',
-        posts,
+        data: { posts },
       });
-    });
-  },
+    })
+    .catch(next);
+};
+
+postController.show = (req, res, next) => {
+  Post.getById(req.params.id)
+    .then((post) => {
+      res.json({
+        message: 'ok',
+        data: { post },
+      });
+    })
+    .catch(next);
+};
+
+postController.create = (req, res, next) => {
+  new Post({
+    text: req.body.text,
+    created_at: req.body.created_at
+  })
+    .save()
+    .then((post) => {
+      res.json({
+        message: 'Post added successfully!',
+        data: { post },
+      });
+    })
+    .catch(next);
+};
+
+postController.update = (req, res, next) => {
+  Post.getById(req.params.id)
+    .then((post) =>
+      post.update({
+        text: req.body.text,
+        created_at: req.body.created_at,
+      })
+    )
+    .then((post) => {
+      res.json({
+        message: 'Post updated successfully!',
+        data: { post },
+      });
+    })
+    .catch(next);
+};
+
+postController.delete = (req, res, next) => {
+  Post.getById(req.params.id)
+    .then((post) => post.delete())
+    .then(() => {
+      res.json({
+        message: 'Post deleted successfully!',
+      });
+    })
+    .catch(next);
 };
 
 module.exports = postController;
