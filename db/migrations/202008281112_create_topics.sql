8cb8e83b1f8a0b31aaecdbec4c9c9259f0d577f6
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-09-01 09:25:40 -0500

    migration syntax updated

--- db/migrations/202008281112_create_topics.sql
+++ db/migrations/202008281112_create_topics.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS topics
 (id SERIAL PRIMARY KEY,
--- title TEXT UNIQUE NOT NULL find the correct command for it,
+title TEXT NOT NULL UNIQUE,
 user_id INTEGER,
 created_at TIMESTAMPTZ,
 board_id INTEGER);
