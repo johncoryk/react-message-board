66d4247bd00460060502564039e949e911a76080
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-09-01 02:02:35 -0500

    posts models done

--- models/Post.js
+++ models/Post.js
@@ -21,6 +21,37 @@ class Post {
       });
   }
 
+  static findById = (id) => {
+    return db.oneOrNone(`
+      SELECT * FROM posts
+      WHERE id = $1
+    `, [id]);
+  }
+
+  static create = (title, created_at) => {
+    return db.one(`
+    INSERT INTO posts
+    (text, created_at)
+    VALUES ($1, $2)
+    RETURNING *
+    `, [post.text, post.created_at]);
+  }
+
+  static update = (text) => {
+    return db.one(`
+    UPDATE posts SET
+    text = $1
+    WHERE id = $2
+    RETURNING *
+    `, [post.text, id]);
+  }
+
+  static delete = (id) => {
+    return db.one(`
+    DELETE FROM posts
+    WHERE id = $1
+    `, [id]);
+  }
   //findbyid
   //create
   //update
