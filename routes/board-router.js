67868cebb719beaddb84de10c2be78403560ae61
Samuel Choi <schoi@Samuels-MBP.attlocal.net>
2020-09-02 09:48:22 -0500

    board router done

--- routes/board-router.js
+++ routes/board-router.js
@@ -1,6 +1,12 @@
+const express = require('express')
 const boardRouter = require('express').Router();
 const boardController = require('../controllers/board-controller');
 
 boardRouter.get('/', boardRouter.index);
+boardRouter.post('/', boardController.create);
+
+boardRouter.get('/:id', boardController.show);
+boardRouter.put('/:id', boardController.update);
+boardRouter.delete('/:id', boardController.delete);
 
 module.exports = boardRouter;
